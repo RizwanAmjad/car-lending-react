86018efbeb7e8ebeb5a7b0561b3df2a28a1b4242
RizwanAmjad <rizwanamjadms@gmail.com>
2022-09-18 18:45:32 +0500

    Done Contact section

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box, Flex } from "@chakra-ui/react"
+import { Flex } from "@chakra-ui/react"
 import { motion } from "framer-motion"
 import TextImage from "../components/TextImage"
 
